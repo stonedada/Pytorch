0c0baec8755bd1dca4df7e6f89f7fd8be1e96265
stone <1406700750@qq.com>
2022-10-12 14:45:40 +0800

    add some new py for test !

---
+++ test/AndOr.py
@@ -0,0 +1,9 @@
+# -*- coding : utf-8 -*-
+# @Author   :   stone
+# @Github   :   https://github.com/stonedada
+print(True and False)
+print(True & False)
+
+
+print(True or False)
+print(True | False)
